0269171609369b26ed413190d0940683fd534ab5
qlh <569374939@qq.com>
2017-03-17 09:01:15 +0800

    20170317

--- admin/article.add.handle.php
+++ admin/article.add.handle.php
@@ -9,11 +9,11 @@
 	$description = $_POST['description'];
 	$content = $_POST['content'];	
 	$dateline = time();
-	$insertsql = "insert into article(title,author,description,content,dateline) values('$title','$author','$description','$content','$dateline')";
-	$jieg=mysql_query('$insertsql');
-	if(mysql_query('$insertsql')){
-		echo "<script>alert('文章发布成功！');window.location.href='article.add.php'</script>"."$jieg";
+	$insertsql = "insert into article(title,author,description,content,dateline) values('$title','$author','$description','$content',$dateline)";
+	if(mysql_query($insertsql)){
+		echo "<script>alert('文章发布成功！');window.location.href='article.add.php'</script>";
 	}else{
 		echo "<script>alert('文章发布失败！');window.location.href='article.add.php'</script>";
+
 	}
 ?>
