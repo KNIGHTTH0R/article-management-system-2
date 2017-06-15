d52bed4d177d351bb3ed5f72aea735e4e2d496d5
qlh <569374939@qq.com>
2017-06-15 09:31:25 +0800

    20170615

--- article.show.php
+++ article.show.php
@@ -9,6 +9,7 @@
 		echo "这篇文章不存在";
 		exit;
 	}
+	
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
